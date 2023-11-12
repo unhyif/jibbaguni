189023d92b4ec293a1cd7822bb7815110ef8ed99
unhyif <ang21icjh@gmail.com>
2023-11-12 17:03:38 +0900

    feat: @supabase/ssr 설치

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,14 +1,32 @@
-import {
-  createClientComponentClient,
-  createServerComponentClient,
-} from '@supabase/auth-helpers-nextjs';
 import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
+import {
+  CookieOptions,
+  createBrowserClient,
+  createServerClient,
+} from '@supabase/ssr';
 import { Database } from '~/types/database/database';
 
-export const clientSupabase = createClientComponentClient<Database>();
+export const clientSupabase = createBrowserClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+);
 export const getServerSupabase = (cookies: ReadonlyRequestCookies) =>
-  createServerComponentClient<Database>({
-    cookies: () => cookies,
-  });
+  createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(name: string) {
+          return cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookies.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookies.delete({ name, ...options });
+        },
+      },
+    },
+  );
 
 export const getCurrentDate = () => new Date().toISOString();
